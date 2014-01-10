1d1c81ea95b64f3d4802ce24cb917a1751aea1f0
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-01-10 08:21:45 -0500

    golfing

--- buddysystem.coffee
+++ buddysystem.coffee
@@ -1,6 +1,4 @@
 buddySystem = (objs) ->
   for o in objs
     s = String o.innerHTML 
-    l = s.match(/\s/g).length - 1
-    r = new RegExp '((?:[^ ]* ){' + l + '}[^ ]*) ' 
-    o.innerHTML = s.replace(r,"$1&nbsp;")
+    o.innerHTML = s.replace((new RegExp '((?:[^ ]* ){' + (s.match(/\s/g).length - 1) + '}[^ ]*) '),"$1&nbsp;")
--- buddysystem.js
+++ buddysystem.js
@@ -1,12 +1,10 @@
 window.buddySystem = function(objs) {
-  var l, o, r, s, _i, _len, _results;
+  var o, s, _i, _len, _results;
   _results = [];
   for (_i = 0, _len = objs.length; _i < _len; _i++) {
     o = objs[_i];
     s = String(o.innerHTML);
-    l = s.match(/\s/g).length - 1;
-    r = new RegExp('((?:[^ ]* ){' + l + '}[^ ]*) ');
-    _results.push(o.innerHTML = s.replace(r, "$1&nbsp;"));
+    _results.push(o.innerHTML = s.replace(new RegExp('((?:[^ ]* ){' + (s.match(/\s/g).length - 1) + '}[^ ]*) '), "$1&nbsp;"));
   }
   return _results;
 };
